e247e5f6330c520ed28376e12449168068c0619b
Jonny Pearson <jonathan.pearson@nhsx.nhs.uk>
2024-12-20 15:15:47 +0000

    minor formatting changes for pre-commit checks

--- src/nhssynth/modules/dataloader/metadata.py
+++ src/nhssynth/modules/dataloader/metadata.py
@@ -80,7 +80,7 @@ class MetaData:
                 roundable_data = data[data.notna()]
                 for i in range(np.finfo(dtype).precision):
                     if (roundable_data.round(i) == roundable_data).all():
-                        return 10 ** -i
+                        return 10**-i
             return None
 
         def _validate_categorical(self, data: pd.Series, categorical: Optional[bool] = None) -> bool:
@@ -321,11 +321,7 @@ class MetaData:
                     "sdtype": (
                         "boolean"
                         if cmd.boolean
-                        else "categorical"
-                        if cmd.categorical
-                        else "datetime"
-                        if cmd.dtype.kind == "M"
-                        else "numerical"
+                        else "categorical" if cmd.categorical else "datetime" if cmd.dtype.kind == "M" else "numerical"
                     ),
                 }
                 for cn, cmd in self._metadata.items()
